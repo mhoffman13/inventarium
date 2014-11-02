e83106a04027445d76ef41b1fbd6d7526494cbc1
mhoffman13 <mhoffman13@student.gsu.edu>
2014-11-02 11:09:17 -0500

    NAME UPDATED! Added char statusCode & applicable methods
    Name updated from Status to EntityStatus to be more descriptive of what this actually is.

---
+++ EntityStatus.java
@@ -0,0 +1,20 @@
+
+public enum EntityStatus {
+	ACTIVE('A'),
+	ARCHIVED('X');
+	
+	private char statusCode;
+	
+	private EntityStatus(char c){
+		statusCode = c;
+	}
+	
+	public char getEntityStatus(){
+		return statusCode;
+	}
+	
+	public static EntityStatus getEntityStatus(char c){
+		String s = Character.toString(c);
+		return EntityStatus.valueOf(s);
+	}
+}
