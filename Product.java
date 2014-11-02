1b38c14c7771d62d57831b008b6547f6dbdd84f2
mhoffman13 <mhoffman13@student.gsu.edu>
2014-11-02 10:55:54 -0500

    Made Category and Vendor obj here sets, added constructors
    Note: I added List <inventories>, but I'm questioning whether we want this on the product object or not. Thoughts?

--- Product.java
+++ Product.java
@@ -1,26 +1,75 @@
+import java.util.List;
+import java.util.Set;
+
 
 public class Product {
-	Integer ID;
+	Integer uniqueId;
 	String name;
+	EntityStatus status;
 	String description;
+	String sku;
 	int quantity;
-	int lowStockQty;
-	Category category;
-	Vendor vendor; 
-	Status status;
+	int lowQuantity;
+	Set <Category> categories;
+	Set <Vendor> vendors;
+	List <Inventory> inventories; // do we want this on the product object?
 	boolean isLow = false;
 	
 	/**
+	 * Constructor
+	 * @param uniqueId
+	 * @param name
+	 * @param status
+	 * @param quantity
+	 */
+	public Product(Integer uniqueId, String name, EntityStatus status,
+			int quantity) {
+		this.uniqueId = uniqueId;
+		this.name = name;
+		this.status = status;
+		this.quantity = quantity;
+	}
+	/**
+	 * Constructor
+	 * @param uniqueId
+	 * @param name
+	 * @param status
+	 * @param description
+	 * @param sku
+	 * @param quantity
+	 * @param lowQuantity
+	 * @param categories
+	 * @param vendors
+	 * @param inventories
+	 */
+	public Product(Integer uniqueId, String name, EntityStatus status,
+			String description, String sku, int quantity, int lowQuantity,
+			Set<Category> categories, Set<Vendor> vendors,
+			List<Inventory> inventories) {
+		this.uniqueId = uniqueId;
+		this.name = name;
+		this.status = status;
+		this.description = description;
+		this.sku = sku;
+		this.quantity = quantity;
+		this.lowQuantity = lowQuantity;
+		this.categories = categories;
+		this.vendors = vendors;
+		this.inventories = inventories;
+		this.isLow = this.isLow();
+	}
+
+	/**
 	 * @return the iD
 	 */
-	public Integer getID() {
-		return ID;
+	public Integer getUniqueId() {
+		return uniqueId;
 	}
 	/**
 	 * @param iD the iD to set
 	 */
-	public void setID(Integer iD) {
-		ID = iD;
+	public void setUniqueId(Integer id) {
+		this.uniqueId = id;
 	}
 	/**
 	 * @return the name
@@ -59,63 +108,57 @@ public class Product {
 		this.quantity = quantity;
 	}
 	/**
-	 * @return the lowStockQty
+	 * @return the lowQuantity
 	 */
-	public int getLowStockQty() {
-		return lowStockQty;
+	public int getlowQuantity() {
+		return lowQuantity;
 	}
 	/**
-	 * @param lowStockQty the lowStockQty to set
+	 * @param lowQuantity the lowQuantity to set
 	 */
-	public void setLowStockQty(int lowStockQty) {
-		this.lowStockQty = lowStockQty;
+	public void setlowQuantity(int lowQuantity) {
+		this.lowQuantity = lowQuantity;
 	}
 	/**
 	 * @return the category
 	 */
-	public Category getCategory() {
-		return category;
+	public Set<Category> getCategories() {
+		return categories;
 	}
 	/**
-	 * @param category the category to set
+	 * @param category the category to add
 	 */
-	public void setCategory(Category category) {
-		this.category = category;
+	public void addCategory(Category category) {
+		this.categories.add(category);
 	}
 	/**
 	 * @return the vendor
 	 */
-	public Vendor getVendor() {
-		return vendor;
+	public Set<Vendor> getVendors() {
+		return vendors;
 	}
 	/**
-	 * @param vendor the vendor to set
+	 * @param vendor the vendor to add
 	 */
-	public void setVendor(Vendor vendor) {
-		this.vendor = vendor;
+	public void addVendor(Vendor vendor) {
+		this.vendors.add(vendor);
 	}
 	/**
 	 * @return the status
 	 */
-	public Status getStatus() {
+	public EntityStatus getStatus() {
 		return status;
 	}
 	/**
 	 * @param status the status to set
 	 */
-	public void setStatus(Status status) {
+	public void setStatus(EntityStatus status) {
 		this.status = status;
 	}
 	/**
-	 * @return the isLow
+	 * @return and set the isLow
 	 */
 	public boolean isLow() {
-		return quantity > lowStockQty;
-	}
-	/**
-	 * @param isLow the isLow to set
-	 */
-	public void setLow(boolean isLow) {
-		this.isLow = isLow;
+		return isLow = quantity <= lowQuantity;
 	}
 }
