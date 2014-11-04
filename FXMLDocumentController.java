f6b4cd426867bae110bf5d6bbe1e00342a360b4a
inventoryUI <inventoryUI@users.noreply.github.com>
2014-11-04 18:52:43 -0500

    Create FXMLDocumentController.java
    It controls the scripts.

---
+++ FXMLDocumentController.java
@@ -0,0 +1,37 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    private Label vendorLabel;
+    
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("Clicked");
+        label.setText("Active");
+    }
+    @FXML
+    private void handleVendorButton(ActionEvent event) {
+        System.out.println("Vendor added");
+        //vendorLabel.setText("Hello Vendor!");
+        label.setText("");
+    }
+     private void mainStock(ActionEvent event) {//show total stock and low stock
+        
+       
+        label.setText("");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+    }    
+    
+}
