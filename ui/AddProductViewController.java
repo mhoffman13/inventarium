b07acdc07471c1b8ea6d16fade604bfe7d358de3
mhoffman13 <mere.hoffman@gmail.com>
2014-11-15 15:49:28 -0500

    indenting changed from spaces to tabs

--- ui/AddProductViewController.java
+++ ui/AddProductViewController.java
@@ -11,30 +11,36 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
-
 public class AddProductViewController implements Initializable {
-    @FXML TextField productName;
-    @FXML TextField productQuantity;
-    @FXML TextField productVendor;
-    @FXML TextField productNumber;
-    @FXML TextField productSKU;
-    @FXML TextField productDescription;
-    
-    
-   @FXML
-   private void handleSaveButton(ActionEvent event) {//method is used when 'Save Item' button is used. It gets text from the Textfields
-        Product product = new Product();
-        product.setName(productName.getText());
-        System.out.println(product.getName());
-        productQuantity.getText();
-        productVendor.getText();
-        productNumber.getText();
-        productSKU.getText();
-        productDescription.getText();
-   
-   }
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
+	@FXML
+	TextField productName;
+	@FXML
+	TextField productQuantity;
+	@FXML
+	TextField productVendor;
+	@FXML
+	TextField productNumber;
+	@FXML
+	TextField productSKU;
+	@FXML
+	TextField productDescription;
+	
+	
+	@FXML
+	private Product handleSaveButton(ActionEvent event) {
+		Product product = new Product();
+		product.setName(productName.getText());
+		System.out.println(product.getName());
+		productQuantity.getText();
+		productVendor.getText();
+		productNumber.getText();
+		productSKU.getText();
+		productDescription.getText();
+		return product;
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+
+	}
 }
