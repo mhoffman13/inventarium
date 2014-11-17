3549257e18b02b02a8333025b4d5f9aa3d58509f
mhoffman13 <mere.hoffman@gmail.com>
2014-11-16 19:28:30 -0500

    added some code to test adding a product

--- ui/AddProductViewController.java
+++ ui/AddProductViewController.java
@@ -1,7 +1,11 @@
 //Variables on AddProductView are controlled here
 package inventarium.ui;
 
+import inventarium.action.EntityActionImpl;
+import inventarium.entity.Category;
 import inventarium.entity.Product;
+import inventarium.entity.Vendor;
+import inventarium.helper.EntityStatus;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,16 +31,30 @@ public class AddProductViewController implements Initializable {
 	
 	
 	@FXML
-	private Product handleSaveButton(ActionEvent event) {
+	private void handleSaveButton(ActionEvent event) {
+		// missing fields in AddProductView.fxml
 		Product product = new Product();
 		product.setName(productName.getText());
-		System.out.println(product.getName());
-		productQuantity.getText();
-		productVendor.getText();
-		productNumber.getText();
-		productSKU.getText();
-		productDescription.getText();
-		return product;
+		product.setQuantity(Integer.parseInt(productQuantity.getText()));
+		product.setSku(productSKU.getText());
+		product.setDescription(productDescription.getText());
+		product.setVendor(new Vendor());
+		product.setCategory(new Category());
+		product.setStatus(EntityStatus.ACTIVE);
+		// prepare to add product
+		EntityActionImpl addAction = new EntityActionImpl(false);
+		if(addAction.addEditEntity(product)){
+			// if successfully added, show product detail view
+			// until the view exists, just print to screen
+			System.out.println("ADDED PRODUCT DATA ...");
+			System.out.println("Name:\t\t" + product.getName());
+			System.out.println("Quantity:\t\t" + product.getQuantity());
+			System.out.println("Sku:\t\t\t" + product.getSku());
+			System.out.println("Description:\t" + product.getDescription());
+			System.out.println("Vendor Id:\t" + product.getVendor().getUniqueId());
+		}else{
+			//something went wrong, display message
+		}
 	}
 
 	@Override
